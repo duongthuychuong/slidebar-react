3ff25a86287bc162ce91e813d75b6b4a134db59f
NCLAN\dtc <duongthuychuong@gmail.com>
2022-11-29 17:48:45 +0700

    create useContext

--- src/context.js
+++ src/context.js
@@ -1,1 +1,18 @@
-import React, { useState, useContext } from 'react'
+import React, { useState, useContext } from "react";
+
+const AppContext = React.createContext();
+
+// const ContextAPI = () => {
+//     const [isModalShowed, setIsModalShowed] = useState(false);
+//     const [isSideBarShowed, setIsSideBarShowed] = useState(false);
+
+// }
+const AppProvider = ({ children }) => {
+  return <AppContext.Provider value="Hello"> {children}</AppContext.Provider>;
+};
+
+const useGlobalContext = () => {
+  return useContext(AppContext);
+};
+
+export { AppProvider, AppContext, useGlobalContext };
